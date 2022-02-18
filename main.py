2c99c2c048e32e29a30882efea9b43cb82a7d624
Liam Alme <92105331+LiamAlme@users.noreply.github.com>
2022-02-18 10:09:52 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,208 @@
+from tkinter import *
 import requests
+from hashlib import blake2b
 
-product_name = input('Enter product name: ')
+def setup():
+    global logged_in
+    logged_in = False
 
-url = 'https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v1'
-payload = {
-    'key': 'ff457966e64d5e877fdbad070f276d18ecec4a01',
-    'channel': 'WEB',
-    'count': '28',
-    'default_purchasability_filter': 'true',
-    'include_sponsored': 'true',
-    'keyword': product_name,
-    'offset': '28',
-    'page': '/s/'+product_name,
-    'platform': 'desktop',
-    'pricing_store_id': '3204',
-    'scheduled_delivery_store_id': '2229',
-    'store_ids': '3204,2229,2300,52,2046',
-    'useragent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
-    'visitor_id': '017EB0D0A3240201BE051346B44DDFF7'
-}
 
-jsonData = requests.get(url, params=payload).json()
+def reset():
+    global e
+    e = blake2b(key=b'DoNotCrackThisKey82394', digest_size=32)
 
-for i in jsonData['data']['search']['products']:
-    print(i['item']['product_description']['title'],i['price']['formatted_current_price'])
+
+def update_visual_list(product):
+    grocery_list.append(product)
+    Label(master, text=grocery_list[len(grocery_list)-1]).grid(row=len(grocery_list)+1,column=0)
+
+
+def target_search(product_name):
+    product_list = []
+
+    #Target has a special product page for Apple products
+    if product_name == 'apple':
+        product_name = 'apples'
+
+    url = 'https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v1'
+    payload = {
+        'key': 'ff457966e64d5e877fdbad070f276d18ecec4a01',
+        'channel': 'WEB',
+        'count': '28',
+        'default_purchasability_filter': 'true',
+        'include_sponsored': 'true',
+        'keyword': product_name,
+        'offset': '28',
+        'page': '/s/'+product_name,
+        'platform': 'desktop',
+        'pricing_store_id': '3204',
+        'scheduled_delivery_store_id': '2229',
+        'store_ids': '3204,2229,2300,52,2046',
+        'useragent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+        'visitor_id': '017EB0D0A3240201BE051346B44DDFF7'
+    }
+
+    jsondata = requests.get(url, params=payload).json()
+
+    for i in jsondata['data']['search']['products']:
+        product_list.append([i['item']['product_description']['title'], i['price']['current_retail']])
+    return product_list
+
+
+def bubble_sort(lst):
+    for k in range(len(lst)):
+        for i in range(len(lst)-1):
+            entry_1 = lst[i]
+            entry_2 = lst[i+1]
+            if entry_1[1] > entry_2[1]:
+                lst[i+1] = lst[i]
+                lst[i] = entry_2
+            elif entry_1[1] == entry_2[1]:
+                if entry_1[0] > entry_2[0]:
+                    lst[i + 1] = lst[i]
+                    lst[i] = entry_2
+    return lst
+
+
+def search_button_command():
+    product = product_name.get()
+    if product != '':
+        win = Tk()
+        win.title('Results')
+
+        product_list = target_search(product)
+        product_list = bubble_sort(product_list)
+
+        for i in range(len(product_list)):
+            product = product_list[i]
+            Label(win, text=product[0]).grid(row=i, column=0)
+            Label(win, text='$'+str(product[1])).grid(row=i, column=1)
+            btn = Button(win, text='Add', command=lambda temp=product: update_visual_list(temp))
+            btn.grid(row=i, column=2)
+
+
+def create_account():
+    username = new_username_entry.get()
+    password = new_password_entry.get()
+    confirm_password = confirm_password_entry.get()
+    flag = False
+    if confirm_password == password:
+        reset()
+        index = 1
+        user_list = open('user_list.txt','r')
+        for line in user_list:
+            if str(username+'\n') == line and index % 2 == 1:
+                found = Tk()
+                found.title('Username Taken')
+                found.geometry("500x500")
+                Label(found, text='Username is Taken').grid()
+                flag = True
+        if not flag:
+            user_list.close()
+            user_list = open('user_list.txt','a')
+            user_list.write(username+'\n')
+            e.update(password.encode())
+            user_list.write(str(e.hexdigest()) + str("\n"))
+            account_created = Tk()
+            account_created.title('Account Created')
+            account_created.geometry('500x500')
+            Label(account_created,text='Account Created').grid()
+            ca.destroy()
+        user_list.close()
+
+
+def create_account_window():
+    global ca
+    ca = Tk()
+    ca.title('Create Account')
+    ca.geometry("500x500")
+
+    Label(ca, text='Username').grid(row=0, column=0)
+    global new_username_entry
+    new_username_entry = Entry(ca)
+    new_username_entry.grid(row=0, column=1)
+
+    Label(ca, text='Password').grid(row=1, column=0)
+    global new_password_entry
+    new_password_entry = Entry(ca)
+    new_password_entry.grid(row=1, column=1)
+
+    Label(ca, text='Confirm Password').grid(row=2, column=0)
+    global confirm_password_entry
+    confirm_password_entry = Entry(ca)
+    confirm_password_entry.grid(row=2, column=1)
+
+    Button(ca, text='Create Account', command=create_account).grid(row=3, column=0)
+
+
+def account_login():
+    global logged_in
+    logged_in = False
+    username = username_entry.get()
+    password = password_entry.get()
+    reset()
+    index = 1
+    index2 = 1
+    flag = 0
+    user_list = open("user_list.txt", "r")
+    for line in user_list:
+        if (str(username) + str("\n")) == str(line):
+            if index % 2 == 1:
+                flag = 1
+                break
+        index += 1
+    if flag == 1:
+        user_list.close()
+        user_list = open("user_list.txt", "r")
+        e.update(password.encode())
+        password = e.hexdigest()
+        for line in user_list:
+            if (str(password) + str("\n")) == str(line):
+                if index + 1 == index2:
+                    user_list.close()
+                    login.destroy()
+                    logged_in = True
+                    break
+            if index2 > index + 1:
+                user_list.close()
+                logged_in = False
+                break
+            index2 += 1
+    else:
+        user_list.close()
+        logged_in = False
+
+
+setup()
+
+if not logged_in:
+    login = Tk()
+    login.title('Login')
+    login.geometry("500x500")
+
+    Label(login, text='Username').grid(row=0, column=0)
+    username_entry = Entry(login)
+    username_entry.grid(row=0, column=1)
+
+    Label(login, text='Password').grid(row=1, column=0)
+    password_entry = Entry(login)
+    password_entry.grid(row=1, column=1)
+
+    Button(login, text='Login', command=account_login).grid(row=2, column=0)
+    Button(login, text='Don\'t have an account? Create one', command=create_account_window).grid(row=3, column=0)
+    mainloop()
+
+if logged_in:
+    master = Tk()
+    master.title('Grocery List')
+    master.geometry("800x600")
+
+    grocery_list = []
+
+    Label(master, text='Grocery List').grid(row=0, column=0)
+    Button(master, text='Search For Item', command=search_button_command).grid(row=1, column=1)
+
+    product_name = Entry(master)
+    product_name.grid(row=1, column=0)
+
+    mainloop()
